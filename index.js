6a6a09cc14f913a44f310504a502349751571070
Sbipul <bipulcroy121@gmail.com>
2022-11-27 00:54:45 +0600

    routing created

--- index.js
+++ index.js
@@ -19,7 +19,27 @@
 
 const http = require("http");
 const server = http.createServer((req, res) => {
-  res.end("hello node js");
+  if (req.url == "/home" || req.url == "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This is home page</p>");
+    res.end();
+  } else if (req.url == "/about") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This is about us page</p>");
+    res.end();
+  } else if (req.url == "/contact") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This is contact page</p>");
+    res.end();
+  } else if (req.url == "details") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This is details page</p>");
+    res.end();
+  } else {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.write("<p>This is error page</p>");
+    res.end();
+  }
 });
 const PORT = 5000;
 server.listen(PORT);
